56d5e04a74b48d0b82f163940bd65ce20f7f496f
Blastius <blas27@gmail.com>
2024-02-07 14:22:48 -0400

    Se creo pagina de notificaciones, se eliminaron campos de mas en pagina de login, sa añadio link de olvido de contraseña.

---
+++ DH_Assets/Pages/Notifications.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DH_Assets.Pages
+{
+    public class NotificationsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
